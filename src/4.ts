eeb8eb587f38bfc8532df5f3e5eafb3c5898da14
Denys07748 <denis07748@gmail.com>
2023-12-13 22:36:42 +0200

    adds task 4

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,54 @@
+class Key {
+    private signature: number;
+
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {}
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean = false;
+    protected tenants: Person[] = [];
+    protected key: Key;
+
+    abstract OpenDoor(key: Key): void;
+
+    comeIn(person: Person): void {
+        if(this.door) {
+            this.tenants.push(person);
+        }
+    }
+}
+
+class MyHouse extends House {
+    constructor(private myKey: Key) {
+        super();
+        this.key = myKey;
+    }
+
+    openDoor(inputKey: Key): void {
+        if (inputKey.getSignature() === this.key.getSignature()) {
+            this.door = true;
+        }
+    }
+
+    OpenDoor(key: Key): void {
+        this.openDoor(key);
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
